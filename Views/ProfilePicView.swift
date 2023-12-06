971cb8af7501234d2e25aac56c400e192188a70c
aabagdi <70984836+aabagdi@users.noreply.github.com>
2023-12-06 12:19:20 +0400

    added profile pic view

--- Views/ProfilePicView.swift
+++ Views/ProfilePicView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Image("logo")
+                Image("logoLight")
                 Spacer()
             }
             Spacer()
